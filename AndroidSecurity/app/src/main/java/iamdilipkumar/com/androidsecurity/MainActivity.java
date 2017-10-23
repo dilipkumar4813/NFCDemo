c732dd24bd9c7c73d205c4dfbc9c183cb00bb445
Dilip <dilipkumar4813@gmail.com>
2017-10-23 07:38:12 +0530

    RSA encryption and decryption, with private key generation PKCS#8

--- AndroidSecurity/app/src/main/java/iamdilipkumar/com/androidsecurity/MainActivity.java
+++ AndroidSecurity/app/src/main/java/iamdilipkumar/com/androidsecurity/MainActivity.java
@@ -2,8 +2,11 @@ package iamdilipkumar.com.androidsecurity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 
 import iamdilipkumar.com.androidsecurity.utils.JwtUtils;
+import iamdilipkumar.com.androidsecurity.utils.RsaUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         JwtUtils.generateJJwt();
+
+        byte[] rsaEncryption = RsaUtils.encrypt("Dilip");
+        Log.d("RSA Encrypt", Base64.encodeToString(rsaEncryption, Base64.DEFAULT));
+
+        byte[] decryptedData = RsaUtils.decrypt(rsaEncryption);
+
+        if (decryptedData != null) {
+            String rsaDecryption = new String(decryptedData);
+            Log.d("RSA Decrypt", rsaDecryption);
+        }
+
+        // Demonstration of building a private key for decryption
+        RsaUtils.buildPrivateKey();
     }
 }
--- AndroidSecurity/app/src/main/java/iamdilipkumar/com/androidsecurity/utils/RsaUtils.java
+++ AndroidSecurity/app/src/main/java/iamdilipkumar/com/androidsecurity/utils/RsaUtils.java
@@ -1,11 +1,23 @@
 package iamdilipkumar.com.androidsecurity.utils;
 
+import android.util.Log;
+
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 /**
  * Class to implement RSA in Android
@@ -16,18 +28,49 @@ import java.security.SecureRandom;
 
 public class RsaUtils {
 
-    static PublicKey pubKey;
-    static PrivateKey privKey;
+    private static PublicKey pubKey;
+    private static PrivateKey privKey;
+
+    private static final String RSA = "RSA";
+
+    private static String rsaModulus = "eeb341f32d52cee219d8b3937bc036ab7b49dc353032ff61adfaed9" +
+            "4657af34650aa8571ba1";
+
+    /**
+     * Method to generate the Private key provided the modulus and exponent
+     *
+     */
+    public static void buildPrivateKey() {
+        BigInteger exponent = new BigInteger("10001",16);
+        BigInteger modulus=new BigInteger(rsaModulus,16);
+        RSAPrivateKeySpec privateSpec = new RSAPrivateKeySpec(modulus, exponent);
+        try {
+
+            KeyFactory factory = KeyFactory.getInstance("RSA");
+            privKey = factory.generatePrivate(privateSpec);
+            Log.d("Private key",privKey.toString());
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Method to initialize RSA Keys
+     */
+    private static void initialize() {
+        if (pubKey == null) {
+            generateRsaKeyPair();
+        }
+    }
 
     /**
      * Method to generate Public and Private key
      * using RSA algorithm
-     *
      */
-    public static void generateRsaKeyPair(){
+    private static void generateRsaKeyPair() {
         try {
-            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-            generator.initialize(512,new SecureRandom());
+            KeyPairGenerator generator = KeyPairGenerator.getInstance(RSA);
+            generator.initialize(512, new SecureRandom());
             KeyPair keyPair = generator.generateKeyPair();
             pubKey = keyPair.getPublic();
             privKey = keyPair.getPrivate();
@@ -36,5 +79,45 @@ public class RsaUtils {
         }
     }
 
+    /**
+     * Method to generate bytes using Cipher class
+     *
+     * @param text - String to be encrypted
+     * @return - bytes
+     */
+    public static byte[] encrypt(String text) {
+        initialize();
+        Log.d("Public key", "" + pubKey.toString());
+        try {
+            Cipher cipher = Cipher.getInstance(RSA);
+            cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+            return cipher.doFinal(text.getBytes());
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException |
+                BadPaddingException | IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
 
+        return null;
+    }
+
+    /**
+     * Bytes to include RSA decryption
+     *
+     * @param src - Bytes to be decrypted
+     * @return - Bytes
+     */
+    public static byte[] decrypt(byte[] src) {
+        initialize();
+        Log.d("Private key", "" + privKey.toString());
+        try {
+            Cipher cipher = Cipher.getInstance(RSA);
+            cipher.init(Cipher.DECRYPT_MODE, privKey);
+            return cipher.doFinal(src);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException |
+                BadPaddingException | IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
